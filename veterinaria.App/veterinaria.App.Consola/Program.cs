7ca5ab4fbeeaf18ee7a4ae918e5e408047bafdd4
BrianGomezM <brayan.gomez2646@gmail.com>
2022-09-11 00:22:42 -0500

    master

---
+++ veterinaria.App/veterinaria.App.Consola/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using veterinaria.App.Dominio;
+using veterinaria.App.Persistencia;
+namespace veterinaria.App.Consola{
+    class Program
+    {
+        private static IRepositorioGato _repoGato = new RepositorioGato(new Persistencia.AppContexto());
+        static void Main(string[] args){
+            
+            Console.WriteLine("Hello World!");
+           AddGato();
+        }
+
+        private static void AddGato(){
+            var gato = new Gato{
+                Nombre = "Pelusa",
+                Raza = "Albino",
+                Edad = 1,
+                Color = "Tigrillo"
+            };
+            _repoGato.AddGato(gato);
+        }
+    }
+}
